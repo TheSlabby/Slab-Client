72ebdd135b76f58b50d170f7bc056a4a8deee3ec
Walker <walker.mcgilvary@gmail.com>
2026-06-20 19:49:45 -0500

    Update version string to 2.0

--- src/main/java/dev/slabstudios/slabclient/SlabClient.java
+++ src/main/java/dev/slabstudios/slabclient/SlabClient.java
@@ -12,7 +12,7 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 @Mod(modid = SlabClient.MODID, version = SlabClient.VERSION)
 public class SlabClient {
 	public static final String MODID = "slabclient";
-	public static final String VERSION = "1.0";
+	public static final String VERSION = "2.0";
 
 	@EventHandler
 	public void init(FMLInitializationEvent event) {
